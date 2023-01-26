4a310fbe5b0181a0f868092c3c6d23524643d8a1
Henry Liu <19509472+henryliu5@users.noreply.github.com>
2023-01-26 16:17:13 -0600

    add parameterization

--- cache.py
+++ cache.py
@@ -13,15 +13,16 @@ two_gb_cache = 500_000_000 # how many 32-bit integers can be held
 features = 100
 CACHE_SIZE = two_gb_cache // features
 
-CACHE_SIZE = CACHE_SIZE // 4
+CACHE_SIZE = CACHE_SIZE // 8
 
 total_evictions = 0
 
 class LRUPolicy:
     def __init__(self, n, preload):
-        for layer_0 in tqdm(preload):
-            for x in layer_0:
-                self._try_get(x.item())
+        ...
+        # for layer_0 in tqdm(preload):
+        #     for x in layer_0:
+        #         self._try_get(x.item())
 
     def try_get(self, x):
         info = self._try_get.cache_info()
@@ -40,30 +41,37 @@ class FIFOPolicy:
         self.n = n
         # TODO fix
         self.s = set()
-        
-        for layer_0 in tqdm(preload):
-            done = False
-            for x in layer_0:
-                self.s.add(x.item())
-                if len(self.s) == n:
-                    done = True
-                    break
-            if done:
-                break
+        self.list = []
+        # for layer_0 in tqdm(preload):
+        #     done = False
+        #     for x in layer_0:
+        #         self.s.add(x.item())
+        #         if len(self.s) == n:
+        #             done = True
+        #             break
+        #     if done:
+        #         break
 
-        self.list = list(self.s) #[x.item() for layer_0 in tqdm(preload) for x in layer_0]
+        # self.list = list(self.s) #[x.item() for layer_0 in tqdm(preload) for x in layer_0]
 
-        if len(self.list) > n:
-            print('warning: list is length', len(self.list), 'but n is:', n)
-
+        # if len(self.list) > n:
+        #     print('warning: list is length', len(self.list), 'but n is:', n)
+        self.test_count = {}
 
     def try_get(self, x):
+        # if x not in self.test_count:
+        #     self.test_count[x] = 0
+        # self.test_count[x] += 1
+
+        global total_evictions
         if x in self.s:
             return True
 
         if len(self.list) == self.n:
             evicted = self.list.pop()     
             self.s.remove(evicted)
+            # print(self.test_count[evicted])
+            total_evictions += 1
 
         self.list.append(x)
         self.s.add(x)
@@ -75,27 +83,28 @@ class MyPolicy:
     def __init__(self, n, preload):
         self.n = n
         self.s = set()
-        
-        for layer_0 in tqdm(preload):
-            done = False
-            for x in layer_0:
-                self.s.add(x.item())
-                if len(self.s) == n:
-                    done = True
-                    break
-            if done:
-                break
-
-        self.list = list(self.s) #[x.item() for layer_0 in tqdm(preload) for x in layer_0]
-
-        if len(self.list) > n:
-            print('warning: list is length', len(self.list), 'but n is:', n)
-
+        self.list = []
         self.count = {}
-        self.update_with_lookahead(preload)
-        self.list.sort(key=lambda x: -self.count[x])
+        # for layer_0 in tqdm(preload):
+        #     done = False
+        #     for x in layer_0:
+        #         self.s.add(x.item())
+        #         if len(self.s) == n:
+        #             done = True
+        #             break
+        #     if done:
+        #         break
 
-        self.evictions_since_update = 0
+        # self.list = list(self.s) #[x.item() for layer_0 in tqdm(preload) for x in layer_0]
+
+        # if len(self.list) > n:
+        #     print('warning: list is length', len(self.list), 'but n is:', n)
+
+        # self.count = {}
+        # self.update_with_lookahead(preload)
+        # self.list.sort(key=lambda x: -self.count[x])
+
+        self.accesses_since_update = 0
 
     def update_with_lookahead(self, new_nodes):
         for layer_0 in new_nodes:
@@ -107,19 +116,18 @@ class MyPolicy:
 
 
     def try_get(self, x):
+        # sort by own count
+        if self.accesses_since_update > 100000:
+            self.list.sort(key=lambda x: -self.count[x])
+            self.accesses_since_update = 0
+        self.accesses_since_update += 1
+
         global total_evictions
 
         if x in self.s:
             return True
 
         if len(self.list) == self.n:
-            self.evictions_since_update += 1
-            # sort by own count
-            if self.evictions_since_update > 100000:
-                self.list.sort(key=lambda x: -self.count[x])
-                print('first:', self.count[self.list[0]], 'last:', self.count[self.list[-1]])
-                self.evictions_since_update = 0
-
             evicted = self.list.pop()
             self.s.remove(evicted)
             total_evictions += 1
@@ -132,50 +140,97 @@ class MyPolicy:
         return False
 
 
-
-def simulate(graph_name, batch_size):
+def simulate(graph_name, batch_size, policy):
+    print('starting simulation with policy', policy)
+    global total_evictions
+    total_evictions = 0
     infile = open(f'{graph_name}_{batch_size}', 'rb')
     nodes = pickle.load(infile)
     infile.close()
 
     # each element is "time" cache miss occurs, # of misses is length
     cache_misses = []
+    num_cache_misses = 0
     cache_total = 0
 
-    cache = LRUPolicy(CACHE_SIZE, preload=nodes[:500])
-    # cache = FIFOPolicy(CACHE_SIZE, preload=nodes[:500])
-    # cache = MyPolicy(CACHE_SIZE, preload=nodes[:500])
-    print('preload done')
-    s = set()
-    time = 0
-    nodes = nodes[500:800]
+    preload = nodes[:500]
+    
+    if policy == 'mine':
+        cache = MyPolicy(CACHE_SIZE, preload=[])
+        cache.update_with_lookahead(preload)
+    elif policy == 'fifo':
+        cache = FIFOPolicy(CACHE_SIZE, preload=[])
+    elif policy == 'lru':
+        cache = LRUPolicy(CACHE_SIZE, preload=[])
 
-    # naive
-    for i, layer_0 in enumerate(tqdm(nodes)):
-        # if i % 20 == 0:
-            # cache.update_with_lookahead(nodes[i:i+20])
-
+    # Warm caches with some queries
+    for layer_0 in tqdm(preload):
         for x in layer_0:
             x = x.item()
             hit = cache.try_get(x)
-            if not hit:
-                cache_misses.append(time)
-            time += 1
-            cache_total += 1
-            s.add(x)
 
+    print('preload done')
+    time = 0
+    nodes = nodes[500:2500]
 
-    print('cache misses:', len(cache_misses), 'total accesses:', cache_total)
-    print('hit ratio:', 1 - (len(cache_misses) / cache_total))
-    print('unique elements', len(s))
+    # naive
+    for i, layer_0 in enumerate(tqdm(nodes)):
+        if policy == '***disabled':
+            lookahead = 5
+            if i % lookahead == 0:
+                cache.update_with_lookahead(nodes[i:i+lookahead])
+
+                scores = {}
+                # Compute the 'score' of each i..i+lookahead query
+                for offset in range(lookahead):
+                    key = i + offset
+                    if key not in scores:
+                        scores[key] = 0
+
+                    for x in nodes[key]:
+                        x = x.item()
+                        scores[key] += cache.count[x]
+
+                execution_order = [i + x for x in range(lookahead)]
+                execution_order.sort(key=lambda x: -scores[x])
+
+                for index in execution_order:
+                    for x in nodes[index]:
+                        x = x.item()
+                        hit = cache.try_get(x)
+                        if not hit:
+                            # cache_misses.append(time)
+                            num_cache_misses += 1
+                        time += 1
+                        cache_total += 1
+        else:
+            if policy == 'mine':
+                lookahead = 100
+                if i % lookahead == 0:
+                    cache.update_with_lookahead(nodes[i:i+lookahead])
+
+            for x in layer_0:
+                x = x.item()
+                hit = cache.try_get(x)
+                if not hit:
+                    # cache_misses.append(time)
+                    num_cache_misses += 1
+                time += 1
+                cache_total += 1
+
+    print('cache misses:', num_cache_misses, 'total accesses:', cache_total)
+    hit_ratio = 1 - (num_cache_misses / cache_total)
+    print('hit ratio:', hit_ratio)
     print('total evictions', total_evictions)
 
-    df = pd.DataFrame(cache_misses, columns=['cache misses over time'])
-    df = df.sample(n=5000, replace=False, random_state=1)
+    # df = pd.DataFrame(cache_misses, columns=['cache misses over time'])
+    # df = df.sample(n=5000, replace=False, random_state=1)
 
-    my_plot = sns.displot(df)
-    my_plot.savefig(f'{graph_name}_{batch_size}.png')
-    plt.show()
+    # my_plot = sns.displot(df)
+    # my_plot.savefig(f'{graph_name}_{batch_size}_{policy}.png')
+    # plt.show()
 
 if __name__ == '__main__':
-    simulate('ogbn-products', 1)
+    simulate('ogbn-products', 1, 'mine')
+    simulate('ogbn-products', 1, 'lru')
+    simulate('ogbn-products', 1, 'fifo')
