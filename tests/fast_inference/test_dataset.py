af6a1da137fd12091c9c00f7bbd9224e7c2356b3
Henry Liu <19509472+henryliu5@users.noreply.github.com>
2023-02-04 07:34:23 +0000

    add tests to verify InferenceDataset partition

--- tests/fast_inference/test_dataset.py
+++ tests/fast_inference/test_dataset.py
@@ -11,3 +11,28 @@ def test_inference_dataset_reload():
     assert(th.all(th.eq(graph1.trace_nids, graph2.trace_nids)))
     assert(th.all(th.eq(graph1.trace_features, graph2.trace_features)))
     # TODO test edges are equal, need to go through list of dictionaries
+
+def test_inference_dataset_partition():
+    infer_data = InferenceDataset('reddit', 1, verbose=True)
+    pruned_g = infer_data[0]
+    print(pruned_g)
+    print(pruned_g.num_nodes())
+    print(infer_data.num_infer_targets)
+
+    # 1. Inference targets will be "present" in the pruned graph to preserve node ids
+    assert(th.all(pruned_g.has_nodes(infer_data.trace_nids)))
+
+    # However, there will be no such edges
+    assert(len(pruned_g.in_edges(infer_data.trace_nids, 'eid')) == 0)
+    assert(len(pruned_g.out_edges(infer_data.trace_nids, 'eid')) == 0)
+    
+    # 2. Inference edges should connect to nodes present in the pruned graph
+    in_nids = th.cat([x["in"] for x in infer_data.trace_edges])
+    out_nids = th.cat([x["out"] for x in infer_data.trace_edges])
+    assert(th.all(pruned_g.has_nodes(in_nids)))
+    assert(th.all(pruned_g.has_nodes(out_nids)))
+
+    # 3. Inference edges should not connect to other inference targets
+    s = set(infer_data.trace_nids.tolist())
+    assert(all(x not in s for x in in_nids))
+    assert(all(x not in s for x in out_nids))
