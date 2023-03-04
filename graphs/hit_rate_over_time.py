61f1b2aef5f57b40b0e3a2c17fb36b2e1076d3eb
Henry Liu <19509472+henryliu5@users.noreply.github.com>
2023-03-04 01:54:08 +0000

    add transfer size graphing, titles for latency and hit rate graphs

--- graphs/hit_rate_over_time.py
+++ graphs/hit_rate_over_time.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 
-def main(paths, policies, model_name, graph_name, batch_size):
+def main(paths, policies, model_name, graph_name, batch_size, file_suffix = ''):
     dfs = []
     for i, path in enumerate(paths):
         df = load_df(model_name + "_cache_info", path, graph_name, batch_size)
@@ -15,18 +15,54 @@ def main(paths, policies, model_name, graph_name, batch_size):
         dfs.append(df)
     cache_df = pd.concat(dfs)
 
+    suffix = "(uniform sampled)"
+    if 'bias' in path:
+        suffix = "(5 subgraphs sampled, bias 0.8)"
+
     g = sns.lineplot(data=cache_df, x=cache_df.index, y='hit_rate', hue='Cache Policy')
-    g.set_title(f'Cache hit rate over 5 partitions (bias 0.8) | {model_name} {graph_name} batch size: {batch_size}')
+    g.set_title(f'Cache hit rate {suffix} | {model_name} {graph_name} batch size: {batch_size}')
     g.set_xlabel('Time (request ID)')
     plt.tight_layout()
-    plt.savefig(f'CROT_{model_name}.png', bbox_inches='tight', dpi=250)
+    plt.savefig(f'CROT{file_suffix}.png', bbox_inches='tight', dpi=250)
+    plt.clf()
+
+    cache_df['transfer_size (MB)'] = (cache_df['request_size'] - cache_df['cache_hits']) * 100 * 4 / 1000 / 1000
+
+    g = sns.lineplot(data=cache_df, x=cache_df.index, y='transfer_size (MB)', hue='Cache Policy')
+    g.set_title(f'Cache hit rate {suffix} | {model_name} {graph_name} batch size: {batch_size}')
+    g.set_xlabel('Time (request ID)')
+    plt.tight_layout()
+    plt.savefig(f'Transfer_size{file_suffix}.png', bbox_inches='tight', dpi=250)
     plt.clf()
     
 if __name__ == '__main__':
     main([
-          'benchmark/data/new_cache_gpu_bias_0.8',
-          'benchmark/data/new_cache_gpu_bias_0.8_count',
-          'benchmark/data/new_cache_gpu_bias_0.8_LFU',
+        #   'benchmark/data/new_cache_gpu_bias_0.8',
+        #   'benchmark/data/new_cache_gpu_bias_0.8_count',
+        #   'benchmark/data/new_cache_gpu_bias_0.8_LFU',
+        #   'benchmark/data/new_cache_gpu_bias_0.8_hybrid',
+        #   'benchmark/data/new_cache_gpu',
+        #   'benchmark/data/new_cache_gpu_count',
+        #   'benchmark/data/new_cache_gpu_LFU',
+        #   'benchmark/data/new_cache_gpu_hybrid',
+
+          'benchmark/fast_data/new_cache_gpu_static',
+          'benchmark/fast_data/new_cache_gpu_count',
+          'benchmark/fast_data/new_cache_gpu_lfu',
+          'benchmark/fast_data/new_cache_gpu_async',
+        #   'benchmark/fast_data/new_cache_gpu_bias_0.8_static',
+        #   'benchmark/fast_data/new_cache_gpu_bias_0.8_count',
+        #   'benchmark/fast_data/new_cache_gpu_bias_0.8_lfu',
+        #   'benchmark/fast_data/new_cache_gpu_bias_0.8_async', 
           ],
-           ['Static (Degree)', 'Counting', 'LFU']
+           ['Static (Degree)', 'Counting', 'LFU', 'Async']
            , 'GCN', 'ogbn-products', 256)
+
+    main([
+          'benchmark/fast_data/new_cache_gpu_bias_0.8_static',
+          'benchmark/fast_data/new_cache_gpu_bias_0.8_count',
+          'benchmark/fast_data/new_cache_gpu_bias_0.8_lfu',
+          'benchmark/fast_data/new_cache_gpu_bias_0.8_async', 
+          ],
+           ['Static (Degree)', 'Counting', 'LFU', 'Async']
+           , 'GCN', 'ogbn-products', 256, f'_biased')
--- graphs/latency_over_time.py
+++ graphs/latency_over_time.py
@@ -7,10 +7,12 @@ import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 
-def main(paths, model_name, graph_name, batch_size):
+def main(paths, model_name, graph_name, batch_size, file_suffix = ''):
     dfs = []
-    for path in paths:
+    policies = ['Baseline (no cache)', 'Static (degree)', 'Counting', 'LFU', 'Async']
+    for i, path in enumerate(paths):
         df = load_df(model_name, path, graph_name, batch_size)
+        df['policy'] = policies[i]
         df['path'] = path
         dfs.append(df)
 
@@ -18,32 +20,73 @@ def main(paths, model_name, graph_name, batch_size):
     df['total (ms)'] = df['total'] * 1000
     print(df)
 
+
+    suffix = "(uniform sampled)"
+    if 'bias' in path:
+        suffix = "(5 subgraphs sampled, bias 0.8)"
+
     # Empirical CDF
-    g = sns.displot(data=df, kind="ecdf", x='total (ms)', hue='path')
-    plt.suptitle(f'Request latency CDF over 5 partitions | {model_name} {graph_name} batch size: {batch_size}')
+    g = sns.displot(data=df, kind="ecdf", x='total (ms)', hue='policy')
+    plt.suptitle(f'Request latency CDF {suffix} | {model_name} {graph_name} batch size: {batch_size}')
+    plt.xlim(0, 120)
     plt.tight_layout()
-    plt.savefig(f'CDF_{model_name}.png', bbox_inches='tight', dpi=250)
+    plt.savefig(f'CDF_{model_name}{file_suffix}.png', bbox_inches='tight', dpi=250)
     plt.clf()
 
     # Lineplot for latency over time
-    g = sns.lineplot(data=df, x=df.index, y='total', hue='path')
-    g.set_title(f'Latency over time over 5 partitions | {model_name} {graph_name} batch size: {batch_size}')
+    g = sns.lineplot(data=df, x=df.index, y='total', hue='policy')
+    g.set_title(f'Latency over time {suffix} | {model_name} {graph_name} batch size: {batch_size}')
     plt.ylim(0, 0.1)
     plt.tight_layout()
     g.set_ylabel('Response time (s)')
     g.set_xlabel('Time (request ID)')
-    plt.savefig(f'LOT_{model_name}.png', bbox_inches='tight', dpi=250)
+    plt.savefig(f'LOT_{model_name}{file_suffix}.png', bbox_inches='tight', dpi=250)
     plt.clf()
 
 if __name__ == '__main__':
     main([
-          'benchmark/data/new_baseline_gpu_bias_0.8',
-          'benchmark/data/new_cache_gpu_bias_0.8',
-        #   'benchmark/data/new_cache_gpu_bias_0.8_only_0',
-        #   'benchmark/data/new_cache_gpu_bias_0.8_only_1',
-        #   'benchmark/data/new_cache_gpu_bias_0.8_only_2',
-          'benchmark/data/new_cache_gpu_bias_0.8_count',
-          'benchmark/data/new_cache_gpu_bias_0.8_LFU',
-        #   'benchmark/data/new_cache_gpu_bias_0.8_count_divide',
-        #   'benchmark/data/new_cache_gpu', 'benchmark/data/new_baseline_gpu'
+        #   'benchmark/data/new_baseline_gpu_bias_0.8',
+        #   'benchmark/data/new_cache_gpu_bias_0.8',
+        # #   'benchmark/data/new_cache_gpu_bias_0.8_only_0',
+        # #   'benchmark/data/new_cache_gpu_bias_0.8_only_1',
+        # #   'benchmark/data/new_cache_gpu_bias_0.8_only_2',
+        #   'benchmark/data/new_cache_gpu_bias_0.8_count',
+        #   'benchmark/data/new_cache_gpu_bias_0.8_LFU',
+        #   'benchmark/data/new_cache_gpu_bias_0.8_hybrid',
+
+          'benchmark/data/new_baseline_gpu',
+          'benchmark/data2/new_cache_gpu_static',
+          'benchmark/data2/new_cache_gpu_count',
+          'benchmark/data2/new_cache_gpu_lfu',
+          'benchmark/data2/new_cache_gpu_async',
+
+        #   'benchmark/data/new_baseline_gpu_bias_0.8',
+        #   'benchmark/data2/new_cache_gpu_bias_0.8_static',
+        #   'benchmark/data2/new_cache_gpu_bias_0.8_count',
+        #   'benchmark/data2/new_cache_gpu_bias_0.8_lfu',
+        #   'benchmark/data2/new_cache_gpu_bias_0.8_async',
           ], 'GCN', 'ogbn-products', 256)
+
+    main([
+          'benchmark/data/new_baseline_gpu_bias_0.8',
+          'benchmark/data2/new_cache_gpu_bias_0.8_static',
+          'benchmark/data2/new_cache_gpu_bias_0.8_count',
+          'benchmark/data2/new_cache_gpu_bias_0.8_lfu',
+          'benchmark/data2/new_cache_gpu_bias_0.8_async',
+          ], 'GCN', 'ogbn-products', 256, '_biased')
+    
+    main([
+          'benchmark/data/new_baseline_gpu',
+          'benchmark/fast_data/new_cache_gpu_static',
+          'benchmark/fast_data/new_cache_gpu_count',
+          'benchmark/fast_data/new_cache_gpu_lfu',
+          'benchmark/fast_data/new_cache_gpu_async',
+          ], 'GCN', 'ogbn-products', 256, '_pinned')
+
+    main([
+          'benchmark/data/new_baseline_gpu_bias_0.8',
+          'benchmark/fast_data/new_cache_gpu_bias_0.8_static',
+          'benchmark/fast_data/new_cache_gpu_bias_0.8_count',
+          'benchmark/fast_data/new_cache_gpu_bias_0.8_lfu',
+          'benchmark/fast_data/new_cache_gpu_bias_0.8_async',
+          ], 'GCN', 'ogbn-products', 256, '_biased_pinned')
